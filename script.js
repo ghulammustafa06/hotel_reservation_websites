6ddecc14425001cc686d8de883b581abbaf2d9c5
Ghulam Mustafa <130176095+ghulammustafa06@users.noreply.github.com>
2024-06-28 22:33:10 +0500

    Update menu with nav-links

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   const menuBtn = document.getElementById('menu-btn');
-  const navMenu = document.getElementById('menu-btn'); 
+  const navMenu = document.getElementById('nav-links'); 
 
   menuBtn.addEventListener('click', function() {
     this.classList.toggle('active');
-    navMenu.classList.toggle('show'); 
+    navMenu.classList.toggle('active'); 
   });
 });
