1ac351d7e337db8ae685fa5ef126bbdef7999109
Ghulam Mustafa <130176095+ghulammustafa06@users.noreply.github.com>
2024-06-30 02:36:47 +0500

    Add sliding effects, using JS in

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     navMenu.classList.toggle('active'); 
   });
 
-   // Sliding cards functionality
+   // Sliding cards functionality for rooms page
    const cardsContainer = document.querySelector('.cards-container');
    let cards = document.querySelectorAll('.card');
 
@@ -23,3 +23,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
    setInterval(slideCards, 3000);
   });
+
+  
+     // Sliding cards functionality for offers page 
+    const cardsContainer = document.querySelector('.offer-container');
+    let cards = document.querySelectorAll('.offer');
+ 
+    function slideCards() {
+        const firstCard = cards[0];
+        cardsContainer.appendChild(firstCard);
+        cards = document.querySelectorAll('.offer');
+        cardsContainer.classList.add('slide');
+        setTimeout(() => {
+            cardsContainer.classList.remove('slide');
+        }, 3000);
+    }
+ 
+    setInterval(slideCards, 3000);
